0161d8c8ab362e83387c53b71c69bbd369a0d10e
Anderson2244 <andersonsouzzaa2@gmail.com>
2024-09-04 17:20:30 -0300

    JS introdução

---
+++ Aula 14/revisao.js
@@ -0,0 +1,57 @@
+let nome = "Guts"
+let idade = 29
+let altura = 1.70
+let temGato = true
+
+
+
+//alert("Olá "+nome+" tudo bem? ")
+
+//console.log(` Olá ${nome} tudo bem? você tem ${idade} anos`)
+
+//let n1 = Number(prompt("Digite um número: "));
+//let n2 = Number(prompt("Digite mais um número: "));
+
+// Calcula a soma
+//let soma = n1 + n2;
+
+// Exibe o resultado com alert
+//alert("A soma é: " + soma);
+
+//let dinheiro = 2500
+
+//if(dinheiro > 5.00){
+    //alert("Posso comprar cigarro")
+//} else if(dinheiro > 1.50){
+    //alert("Uma bala")
+//} else {
+    //alert("Não posso comprar")
+//}
+
+
+
+//let n1 = Number(prompt("Digite um número: "));
+//let n2 = Number(prompt("Digite mais um número: "));
+//let n3 = Number(prompt("Digite mais um número: "));
+
+//let media = (n1 + n2 + n3) / 3
+//let mediaArrendondar = media.toFixed(2)
+
+//alert("A média é: " + mediaArrendondar);
+
+//let contador = 1
+//while(contador <= 10){
+   // console.log(contador)
+    //contador++
+//}
+
+//for(contador = 1; contador <= 10;contador++){
+    //console.log(contador)
+//}
+
+function ola(nome){
+    console.log("Ola"+nome)
+}
+    ola(" Gabriel")
+    ola(" Anderson")
+    ola(" José")
---
+++ Aula 14/script.js
@@ -0,0 +1,19 @@
+function clique(){
+    alert("Fui clicado!")
+}
+
+function FocusMe(){
+    console.log("Fui focado!")
+}
+
+function BlurMe(){
+    console.log("Fui desfocado!")
+}
+
+function changeMe(){
+    console.log("Fui alterado!")
+}
+
+function OverMe(){
+    console.log("Mouse aqui!")
+}
