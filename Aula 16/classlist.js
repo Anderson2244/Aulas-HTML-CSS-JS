89a850dd26139918c6399085e23d7aeb537095cb
Anderson2244 <andersonsouzzaa2@gmail.com>
2024-09-09 17:21:46 -0300

    elementos js

---
+++ Aula 16/classlist.js
@@ -0,0 +1,27 @@
+function minhaFuncao(){
+    document.querySelector('.minhaDiv').classList.add("classe1","classe2","classe3")
+}
+
+function minhaFuncao2(){
+    document.querySelector('.minhaDiv').classList.remove("classe1","classe2","classe3")
+}
+
+function fundoAzul(){
+    let elemento = document.querySelector('#style')
+    elemento.style.backgroundColor = "blue"
+}
+
+function fundoTrans(){
+    let elemento = document.querySelector('#style')
+    
+}
+
+function fonteVer(){
+    let elemento = document.querySelector('#style')
+
+}
+
+function fonteNor(){
+    let elemento = document.querySelector('#style')
+
+}
